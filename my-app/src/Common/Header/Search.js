e1bfca8340fefee32b983f059e1719453fdf293c
sakibsalehcse <sakibsalehcse@gmail.com>
2023-03-09 21:56:59 +0600

    Search component created

--- my-app/src/Common/Header/Search.js
+++ my-app/src/Common/Header/Search.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { BsSearch } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+import { GiBeachBag } from "react-icons/gi";
 
 const Search = () => {
   return (
-    <div>
-      Search
+    <div className='search'>
+      <h1 style={{ fontStyle: "italic", fontWeight: "bold" }}>বণিক</h1>
+      <div className='flex'>
+        <div className='searchBar'>
+          <BsSearch />
+          <input type="text" placeholder='Search and hit enter ...' />
+        </div>
+        <div>
+          <span>All Category</span>
+        </div>
+      </div>
+      <div className='user'>
+        <span className='icon'><FaUserAlt /></span>
+        <span className='icon'> <GiBeachBag /></span>
+        <label className='label'></label>
+      </div>
     </div>
   )
 }
 
-export default Search
+export default Search;
