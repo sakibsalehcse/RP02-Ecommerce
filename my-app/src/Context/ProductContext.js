c318391723339a86159ebdf5ab5589caa68e401d
sakibsalehcse <sakibsalehcse@gmail.com>
2023-03-11 19:52:04 +0600

    dispatch method created and it will call the action method of Reducer function

--- my-app/src/Context/ProductContext.js
+++ my-app/src/Context/ProductContext.js
@@ -1,5 +1,6 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
+import ProductReducer from "../Reducer/ProductReducer";
 // provider
 // consumer => useContext Hook
 //product warehouse
@@ -8,15 +9,30 @@ import axios from "axios";
 const AppContext = createContext();
 const Api = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts: []
+};
 
 const AppProvider = ({ children }) => {
 
+    const [state, dispatch] = useReducer(ProductReducer, initialState);
 
     const getProducts = async (ApiUrl) => { // actually passing Api here getProduct(Api)  its actually returns promises  thats why async is used
-        const resDataFromApi = await axios.get(ApiUrl);   // await also used for the same reason coz its returning promise insted of .then .then .catch we  use async and await
-        console.log(resDataFromApi); // thats for checking that we are getting data from APi or not
-        const products =await resDataFromApi.data; // product data from APi
-        console.log(products);
+        dispatch({type:"Loading"});
+        try {
+            const resDataFromApi = await axios.get(ApiUrl);   // await also used for the same reason coz its returning promise insted of .then .then .catch we  use async and await
+            console.log(resDataFromApi); // thats for checking that we are getting data from APi or not
+            const products = await resDataFromApi.data; // product data from APi
+            console.log(products);
+            dispatch({ type: "MY_API_DATA", payload: products });
+        }
+        catch(error) {
+             console.error(error);
+             dispatch({type:"API_ERROR"});
+         }
     }
 
     useEffect(() => {
@@ -24,7 +40,8 @@ const AppProvider = ({ children }) => {
     }, []); // [] this is for array dependencies
 
     return (
-        <AppContext.Provider value={{ GlobalProductName: "Iphone" }}>{children}</AppContext.Provider>
+
+        <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider> //...state actually we add all data here
         // Product is work like a deliveryboy and value is a product
     )
 }
---
+++ my-app/src/Reducer/ProductReducer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ProductReducer = (state,action) => {
+        return state;
+};
+
+export default ProductReducer;
